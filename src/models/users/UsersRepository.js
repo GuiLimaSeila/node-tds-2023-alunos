f8cb9e9ca0539c63465aa2f700e7f06e516e2658
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2024-04-17 08:04:30 -0300

    feat: template data base

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -6,44 +6,74 @@ export default class UsersRepository {
   }
 
   async getUsers() {
-    const allUsers = await this.db.query("SELECT * FROM users");
+    try {
+      const allUsers = await this.db.query("SELECT * FROM users");
 
-    return allUsers;
+      return allUsers;
+    } catch (error) {
+      console.error("Error on getUsers: ", error);
+      throw error;
+    }
   }
 
   async getUserById(id) {
-    const user = await this.db.oneOrNone("SELECT * FROM users WHERE id = $1", 
-    id
-    );
+    try {
+      const user = await this.db.oneOrNone("SELECT * FROM users WHERE id = $1",
+        id
+      );
 
-    return user;
+      return user;
+    } catch (error) {
+      console.error("Error on getUserById: ", error);
+      throw error;
+    }
   }
 
-   async getUserByEmail(email) {
-    const user = await this.db.oneOrNone("SELECT * FROM users WHERE email = $1", 
-    email
-    );
+  async getUserByEmail(email) {
+    try {
+      const user = await this.db.oneOrNone("SELECT * FROM users WHERE email = $1",
+        email
+      );
 
-    return user;
+      return user;
+    } catch (error) {
+      console.error("Error on getUserByEmail: ", error);
+      throw error;
+    }
   }
 
   async createUser(user) {
-    await this.db.none(
-      "INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)",
-      [user.id, user.name, user.email, user.password]
-    );
+    try {
+      await this.db.none(
+        "INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)",
+        [user.id, user.name, user.email, user.password]
+      );
+    } catch (error) {
+      console.error("Error on createUser: ", error);
+      throw error;
+    }
   }
 
   async updateUser(id, name, email, password) {
-   const updateUser = await this.db.one(
-      "UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4 RETURNING *",
-      [name, email, password, id]
-    );
+    try {
+      const updateUser = await this.db.one(
+        "UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4 RETURNING *",
+        [name, email, password, id]
+      );
 
-    return updateUser;
+      return updateUser;
+    } catch (error) {
+      console.error("Error on updateUser: ", error);
+      throw error;
+    }
   }
 
   async deleteUser(id) {
-    await this.db.none("DELETE FROM users WHERE id = $1", id);
+    try {
+      await this.db.none("DELETE FROM users WHERE id = $1", id);
+    } catch (error) {
+      console.error("Error on deleteUser: ", error);
+      throw error;
+    }
   }
 }
